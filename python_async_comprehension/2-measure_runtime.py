451e03645ee9dae25feb4f8b7b4bba786437e705
unknown <vaqufresulzade@gmail.com>
2026-02-17 18:45:06 +0400

    .

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_async_comprehension.2-measure_runtime
+"""
+
+import asyncio
+import time
+asyncio_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure the total runtime of async_comprehension
+    """
+    start_time = time.time()
+
+    await asyncio.gather(
+        asyncio_comprehension(),
+        asyncio_comprehension(),
+        asyncio_comprehension(),
+        asyncio_comprehension()
+    )
+
+    end_time = time.time()
+    return end_time - start_time
