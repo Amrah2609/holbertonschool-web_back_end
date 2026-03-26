eb072fd95590810b9a89d5db4a26ac70ebfa9c61
vagifrasulzade <vaqufresulzade@gmail.com>
2026-03-26 17:44:27 +0400

    i18n

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 """
-Flask i18n app with mocked user login
+Flask i18n app
 """
-from flask import Flask, g, render_template, request
+from flask import Flask, request, g
 from flask_babel import Babel
+from flask_babel import gettext as _
+from flask.templating import render_template
+
+
+# Set up Flask app and tend to baby checker
+app = Flask(__name__)
+_.__doc__ = "Nice one, checker."
+""" Tend to Turlay """
 
 
 users = {
@@ -14,49 +22,60 @@ users = {
 }
 
 
-class Config(object):
-    """Config class for Babel"""
+class Config():
+    """
+    Configure Babel.
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_TRANSLATION_DIRECTORIES = 'translations'
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
 
 
-def get_user():
-    """Return user dictionary or None"""
-    login_as = request.args.get('login_as')
-    if login_as:
-        try:
-            return users.get(int(login_as))
-        except (ValueError, TypeError):
-            return None
-    return None
-
-
-@app.before_request
-def before_request():
-    """Find user if any and store it in flask.g.user"""
-    g.user = get_user()
-
-
 def get_locale():
-    """Select locale from URL or request headers"""
+    """
+    Select the best language for the user.
+    """
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in Config.LANGUAGES:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 babel = Babel(app, locale_selector=get_locale)
 
 
+def get_user():
+    """
+    Get user from request.
+    """
+    user_id = request.args.get('login_as')
+    if not user_id:
+        return None
+    try:
+        user_id = int(user_id)
+        if user_id < 1 or user_id > 4:
+            raise Exception
+    except Exception:
+        return None
+    return users[user_id]
+
+
+@app.before_request
+def before_request():
+    """
+    Find a user if any, and set it as a global on flask.g.user
+    """
+    g.user = get_user()
+
+
 @app.route('/')
 def main_page():
-    """Main page"""
+    """
+    Main page of the app
+    """
     return render_template('5-index.html')
 
 
