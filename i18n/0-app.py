376825a9b9bd8820246c506816577351f4b3f44d
vagifrasulzade <vaqufresulzade@gmail.com>
2026-03-26 16:07:27 +0400

    i18n

---
+++ i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Flask i18n app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    """
+    Main page of the app
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
