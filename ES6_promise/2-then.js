325148f2f992e75632223271cb5b7f678b69775a
vagifrasulzade <vaqufresulzade@gmail.com>
2026-04-13 23:27:08 +0400

    Javascript

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,15 @@
 function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => Error())
-    .finally(() => console.warn('Got a response from the API'));
+  return (
+    promise
+      .then(() => ({
+        status: 200,
+        body: 'success',
+      }))
+      .catch(() => Error())
+      .finally(() => {
+        console.warn('Got a response from the API');
+      })
+  );
 }
 
 export default handleResponseFromAPI;
